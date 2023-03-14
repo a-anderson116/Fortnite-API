2f4c5fcef1e9a44abd20bb6fee3c0e3fbad37e4e
Alex Anderson <andersonaj116@gmail.com>
2023-03-14 10:09:01 -0500

    Have  a list of regions

--- Fortnite API/ContentView.swift
+++ Fortnite API/ContentView.swift
@@ -11,100 +11,111 @@ import Foundation
 struct ContentView: View {
     @State var eventList: Dictionary<String, String>
     @State var TourneyData = ""
+    @State var data = [Data]()
+    let Regions = [ String("NAE"), String("NAW"), String("NAC"), String("ME"), String("EU"), String("ASIA"), String("OCE"), String("BR")]
+    @State private var showingAlert = false
+    
     var body: some View {
         NavigationView{
-                    VStack{
-                        
-                        Text(TourneyData).font(.system(size: 5))
-                        
-                        
-                    }
-                  }
-        .task{
+            List(Regions,id: \.self){
+                region in NavigationLink(destination: Region_View()){
+                    Text(region)
+                }
+            }
+            
+            .task{
                 await GetData()
                 
             }
-              }
-            
-    func GetData() async{
-        let apiUrl = "https://fortniteapi.io/v1/events/list?lang=en&region=All&showArena=false"
-
-        // Create a URL object from the string =
-        let url = URL(string: apiUrl)
-
-        // Create a URL request object
-        var request = URLRequest(url: url!)
-
-        // Set the HTTP method to GET
-        request.httpMethod = "GET"
-
-        // Add the API key as an HTTP header
-        let apiKey = "30492435-463fceb0-62064001-574ba7d3"
-        request.addValue(apiKey, forHTTPHeaderField: "Authorization")
-
-        // Create a URL session object
-        let session = URLSession.shared
-
-        // Create a data task to retrieve data from the API
-        let task = session.dataTask(with: request) { data, response, error in
-            // Check for errors
-            guard error == nil else {
-                print("Error: \(error!)")
-                return
-            }
-            
-            // Check for response status code
-            guard let httpResponse = response as? HTTPURLResponse,
-                  (200...299).contains(httpResponse.statusCode) else {
-                print("Invalid response")
-                return
-            }
-            
-            // Check for data
-            guard let jsonData = data else {
-                print("No data returned")
-                return
-            }
-            
-            // Parse the JSON data using JSONDecoder
-            let decoder = JSONDecoder()
-            
-            do {
-                let decodedData = try decoder.decode(Events.self, from: jsonData)
-                print("Data: \(decodedData)")
-                TourneyData = "Data: \(decodedData)"
-            } catch {
-                print("Error decoding JSON data: \(error)")
+            .alert(isPresented: $showingAlert){
+                Alert(title: Text ("Loading Error"),
+                      message:Text("There was a problem loading the API Categories"),
+                      dismissButton: .default(Text("OK"))
+                )
             }
         }
-        task.resume()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView(eventList: ["" : ""])
+        func GetData() async{
+            let apiUrl = "https://fortniteapi.io/v1/events/list?lang=en&region=All&showArena=false"
+            
+            // Create a URL object from the string =
+            let url = URL(string: apiUrl)
+            
+            // Create a URL request object
+            var request = URLRequest(url: url!)
+            
+            // Set the HTTP method to GET
+            request.httpMethod = "GET"
+            
+            // Add the API key as an HTTP header
+            let apiKey = "30492435-463fceb0-62064001-574ba7d3"
+            request.addValue(apiKey, forHTTPHeaderField: "Authorization")
+            
+            // Create a URL session object
+            let session = URLSession.shared
+            
+            // Create a data task to retrieve data from the API
+            let task = session.dataTask(with: request) { data, response, error in
+                // Check for errors
+                guard error == nil else {
+                    print("Error: \(error!)")
+                    return
+                }
+                
+                // Check for response status code
+                guard let httpResponse = response as? HTTPURLResponse,
+                      (200...299).contains(httpResponse.statusCode) else {
+                    print("Invalid response")
+                    return
+                }
+                
+                // Check for data
+                guard let jsonData = data else {
+                    print("No data returned")
+                    return
+                }
+                
+                // Parse the JSON data using JSONDecoder
+                let decoder = JSONDecoder()
+                
+                do {
+                    let decodedData = try decoder.decode(Events.self, from: jsonData)
+                    print("Data: \(decodedData)")
+                    TourneyData = "Data: \(decodedData)"
+                } catch {
+                    print("Error decoding JSON data: \(error)")
+                }
+            }
+            task.resume()
+        }
     }
-}
-struct Events: Codable {
-    var events: [Data]
     
-    enum CodingKeys : String, CodingKey{
-        case events = "events"
-       
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView(eventList: ["" : ""])
+        }
     }
-}
-struct Data: Identifiable, Codable{
-    var id = UUID()
-    var name: String
-    var mode: String
-    
-    enum CodingKeys : String, CodingKey{
-        case name = "name_line1"
-        case mode = "name_line2"
+    struct Events: Codable {
+        var events: [Data]
         
+        enum CodingKeys : String, CodingKey{
+            case events = "events"
+            
+        }
     }
-}
-
-
+    struct Data: Identifiable, Codable{
+        var id = UUID()
+        var name: String
+        var mode: String
+        
+        enum CodingKeys : String, CodingKey{
+            case name = "name_line1"
+            case mode = "name_line2"
+            
+            
+        }
+    }
+    
+    
+    
 
---
+++ Fortnite API/Region View.swift
@@ -0,0 +1,20 @@
+//
+//  Region View.swift
+//  Fortnite API
+//
+//  Created by Alex Anderson on 3/14/23.
+//
+
+import SwiftUI
+
+struct Region_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Region_View_Previews: PreviewProvider {
+    static var previews: some View {
+        Region_View()
+    }
+}
